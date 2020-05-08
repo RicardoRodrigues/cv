73bedeb1aef91258fbc2426564f6259b9089b0d4
Ricardo Rodrigues <r7rodrigues@gmail.com>
2020-05-08 14:50:40 +0100

    feat: hello world for github-pages
    Setup all deploy process github page using react and next

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+class Home extends React.Component {
+  render () {
+    return <div>Hello world!</div>
+  }
+}
+
+export default Home;
