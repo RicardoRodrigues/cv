0d6ec61c86010ead78e8fbe41cbe8e37a5d33102
Ricardo Rodrigues <r7rodrigues@gmail.com>
2020-05-17 20:16:29 +0100

    Updates

---
+++ _next/static/IaVHQ44xF-msIUXiAo-Cj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[b,a],"/_error":[a],"/index":[b,a]}}("static\u002Fchunks\u002F3c21a328c47a68751d950059c07fbf19470961ab.5642860ac06b9ee94e0b.js","static\u002Fchunks\u002F1bfc9850.a942f9fd878da824bb76.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
